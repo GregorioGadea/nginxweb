9bd7917e351cab64fe1bea5ee10bc4849a703a0f
Gregório Gadea <gregoriogadea.sl026@academico.ifsul.edu.br>
2023-07-21 13:38:49 -0300

    atualizando script

--- web/user-data-web.sh
+++ web/user-data-web.sh
@@ -13,11 +13,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 
 sleep 10 # wait for docker-compose to download
 
-sudo chmod +x /usr/local/bin/docker-compose 
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo systemctl start docker
-
-sleep 5
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo cat <<"EOF" > /home/ubuntu/compose_files/docker-compose.yml
 version: '3'
